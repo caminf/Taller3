2c14927d3ddf281dea862dd5305ac85a69b046e5
caminf <c.infante01@ufromail.cl>
2023-10-07 00:06:14 -0300

    Styling
    Fix of event list and header styiling

--- src/components/Event.vue
+++ src/components/Event.vue
@@ -1,18 +1,18 @@
 <template>
     <v-sheet class="d-flex ma-1">
         <v-list lines="one" style="color:#ffff; background-color: #003EAA;">
-            <v-list-item height="20px" width="1050px" style="justify-content: space-between;">
+            <v-list-item height="20px" width="1500px" style="justify-content: space-between;">
                 <v-row>
                     <v-col cols="1">
-                        <h6>{{ props.day }} - {{ props.month }}</h6>
-                        <h6>{{ props.year }}</h6>
+                        <h5>{{ props.day }} - {{ props.month }}</h5>
+                        <h5>{{ props.year }}</h5>
                     </v-col>
-                    <v-col cols="9">
-                        <h5>{{ props.title }}</h5>
+                    <v-col cols="10">
+                        <h4>{{ props.title }}</h4>
                     </v-col>
-                    <v-col cols="2">
+                    <v-col cols="1">
                         <v-btn :href=props.source variant="text" density="comfortable" color="#ACD0FF">
-                            <h5>Ver evento</h5>
+                            <h4>Ver evento</h4>
                         </v-btn>
                     </v-col>
                 </v-row>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,17 +3,17 @@
     <v-row style="justify-content: space-between;">
       <v-col cols="2">
         <v-img src="https://www.ufro.cl/index.php/normas-corporativas/32-logo-ufro-blanco-horizontal/file" class="ma-2"
-          height="40px"></v-img>
+          height="60px"></v-img>
       </v-col>
       <v-col cols="3" style="align-self: center;">
         <router-link class="button" to="/">
           <v-btn class="" color="#ACD0FF" density="comfortable" variant="text">
-            <h5>Noticias</h5>
+            <h4>Noticias</h4>
           </v-btn>
         </router-link>
         <router-link class="button" to="/events">
           <v-btn color="#ACD0FF" density="comfortable" variant="text">
-            <h5>Eventos</h5>
+            <h4>Eventos</h4>
           </v-btn>
         </router-link>
       </v-col>
